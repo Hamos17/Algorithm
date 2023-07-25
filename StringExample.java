e7922f67b93bc796bc5bb725bbecd772a450817c
Hamos17 <gksmfwm1000@gmail.com>
2023-07-26 00:23:49 +0900

    String

---
+++ StringExample.java
@@ -0,0 +1,20 @@
+
+/*
+ *  [ Java.lang.String ]
+ * - Java.lang 패키지로 제공되는 Java 문자열 클래스
+ * - 별도의 import 없이 사용 가능
+ * - 문자열 작업에 유용한 메소드 제공
+ * - 한 번 인스턴스가 생성되면 수정할 수 없음 (immutable object)
+ */
+
+public class StringExample {
+
+	public static void main(StringExample[] args) {
+		
+		String str1 = "Hello";
+		String str2 = "World!";
+		
+		String str3 = new String("Java");
+		String str4 = new String("C#");
+	}
+}
